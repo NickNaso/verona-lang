5f697889980cdbe4421a31b487afae6af2fb8450
NickNaso <nicoladelgobbo@gmail.com>
2020-01-23 11:29:11 +0100

    Added comment example.

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 echo Start exectuing examples ... && \
 ./verona/build_ninja/dist/veronac --run hello.verona && \
+./verona/build_ninja/dist/veronac --run comment.verona && \
 echo && \
 echo Examples successfully executed.
