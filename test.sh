7397c91b88712923dd17983bb06218251dd290f7
NickNaso <nicoladelgobbo@gmail.com>
2020-01-27 10:45:46 +0100

    Starde workning on new example for integer type.

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 echo Start exectuing examples ... && \
 ./verona/build_ninja/dist/veronac --run hello.verona && \
 ./verona/build_ninja/dist/veronac --run comment.verona && \
+./verona/build_ninja/dist/veronac --run integer.verona && \
 echo && \
 echo Examples successfully executed.
